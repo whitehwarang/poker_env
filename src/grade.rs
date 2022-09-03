5be356ab96ae8d35f8edbdc676b07f4ad9268d3b
whitehwarang <hwarangmun@nate.com>
2022-09-03 17:55:46 +0900

    failed

--- src/grade.rs
+++ src/grade.rs
@@ -6,16 +6,16 @@ use std::cmp::Ordering;
 use std::fmt;
 
 #[derive(PartialEq)]
-pub enum Grade {
-    StraightFlush((usize, usize, usize, usize, usize)),
-    FourOfAKind((usize, usize, usize, usize, usize)),
-    FullHouse((usize, usize, usize, usize, usize)),
-    Flush((usize, usize, usize, usize, usize)),
-    Straight((usize, usize, usize, usize, usize)),
-    ThreeOfAKind((usize, usize, usize, usize, usize)),
-    TwoPairs((usize, usize, usize, usize, usize)),
-    OnePair((usize, usize, usize, usize, usize)),
-    Top((usize, usize, usize, usize, usize)),
+pub enum Grade<'a> {
+    StraightFlush((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
+    FourOfAKind((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
+    FullHouse((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
+    Flush((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
+    Straight((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
+    ThreeOfAKind((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
+    TwoPairs((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
+    OnePair((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
+    Top((&'a Card, &'a Card, &'a Card, &'a Card, &'a Card)),
 }
 
 impl PartialOrd for Grade {
@@ -30,7 +30,7 @@ impl fmt::Display for Grade {
     }
 }
 
-impl Grade {
+impl<'a> Grade {
     pub fn to_string(&self) -> String {
         match self {
             Self::StraightFlush(tuple) => format!("Straight Flush	: {:?}", tuple),
@@ -47,15 +47,15 @@ impl Grade {
 
     fn as_value(&self) -> (u8, usize, usize, usize, usize, usize) {
         match self {
-            Self::StraightFlush((a, b, c, d, e)) => (8, *a, *b, *c, *d, *e),
-            Self::FourOfAKind((a, b, c, d, e)) => (7, *a, *b, *c, *d, *e),
-            Self::FullHouse((a, b, c, d, e)) => (6, *a, *b, *c, *d, *e),
-            Self::Flush((a, b, c, d, e)) => (5, *a, *b, *c, *d, *e),
-            Self::Straight((a, b, c, d, e)) => (4, *a, *b, *c, *d, *e),
-            Self::ThreeOfAKind((a, b, c, d, e)) => (3, *a, *b, *c, *d, *e),
-            Self::TwoPairs((a, b, c, d, e)) => (2, *a, *b, *c, *d, *e),
-            Self::OnePair((a, b, c, d, e)) => (1, *a, *b, *c, *d, *e),
-            Self::Top((a, b, c, d, e)) => (0, *a, *b, *c, *d, *e),
+            Self::StraightFlush((a, b, c, d, e)) => (8, (*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
+            Self::FourOfAKind((a, b, c, d, e)) => (7, (*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
+            Self::FullHouse((a, b, c, d, e)) => (6, (*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
+            Self::Flush((a, b, c, d, e)) => (5, (*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
+            Self::Straight((a, b, c, d, e)) => (4, (*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
+            Self::ThreeOfAKind((a, b, c, d, e)) => (3,(*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
+            Self::TwoPairs((a, b, c, d, e)) => (2, (*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
+            Self::OnePair((a, b, c, d, e)) => (1, (*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
+            Self::Top((a, b, c, d, e)) => (0, (*a).num, (*b).num, (*c).num, (*d).num, (*e).num),
         }
     } // end :: as_value()
 
@@ -63,37 +63,38 @@ impl Grade {
         let mut cards: Vec<Card> = cards.clone().to_vec();
         cards.sort();
         cards.reverse();
-        if let Some(sf) = Self::is_straight_flush(&cards) {
+        let shape_cnts = Self::shape_counts(&cards);
+        let num_cnts   = Self::num_counts(&cards);
+        if let Some(sf) = Self::is_straight_flush(&cards, &shape_cnts, &num_cnts) {
             return sf;
         }
-        if let Some(fk) = Self::is_four_of_a_kind(&cards) {
+        if let Some(fk) = Self::is_four_of_a_kind(&cards, &shape_cnts, &num_cnts) {
             return fk;
         }
-        if let Some(fh) = Self::is_full_house(&cards) {
+        if let Some(fh) = Self::is_full_house(&cards, &shape_cnts, &num_cnts) {
             return fh;
         }
-        if let Some(fl) = Self::is_flush(&cards) {
+        if let Some(fl) = Self::is_flush(&cards, &shape_cnts, &num_cnts) {
             return fl;
         }
-        if let Some(st) = Self::is_straight(&cards) {
+        if let Some(st) = Self::is_straight(&cards, &num_cnts) {
             return st;
         }
-        if let Some(tk) = Self::is_three_of_a_kind(&cards) {
+        if let Some(tk) = Self::is_three_of_a_kind(&cards, &shape_cnts, &num_cnts) {
             return tk;
         }
-        if let Some(tp) = Self::is_two_pairs(&cards) {
+        if let Some(tp) = Self::is_two_pairs(&cards, &shape_cnts, &num_cnts) {
             return tp;
         }
-        if let Some(op) = Self::is_one_pair(&cards) {
+        if let Some(op) = Self::is_one_pair(&cards, &shape_cnts, &num_cnts) {
             return op;
         }
         Self::Top(Self::extract_top5(&cards))
     } // end :: new()
 
-    fn extract_top5(cards: &[Card]) -> (usize, usize, usize, usize, usize) {
+    fn extract_top5(cards: &[Card]) -> (&'a Card, &'a Card, &'a Card, &'a Card, &'a Card) {
         cards
             .iter()
-            .map(|&card| card.num)
             .take(5)
             .collect_tuple()
             .unwrap()
@@ -136,13 +137,12 @@ impl Grade {
         cnts
     }
 
-    fn is_straight_flush(cards: &[Card]) -> Option<Self> {
+    fn is_straight_flush(cards: &[Card], shape_cnts: &[usize; 4], num_cnts: &[usize; 13]) -> Option<Self> {
         // search a major shape of the cards.
-        let shape_cnts = Self::shape_counts(cards);
         let sh_cnt: Option<_> = shape_cnts
             .into_iter()
             .enumerate()
-            .filter(|&(_shape, shape_cnt)| shape_cnt >= 5)
+            .filter(|&(_shape, shape_cnt)| *shape_cnt >= 5)
             .nth(0);
         if sh_cnt.is_none() {
             return None;
@@ -157,103 +157,90 @@ impl Grade {
             .collect::<Vec<Card>>();
 
         // reuse a code for checking if it is straight.
-        match Self::is_straight(&shape_filtered_cards) {
-            Some(Self::Straight(nums)) => Some(Self::StraightFlush(nums)),
+        match Self::is_straight(&shape_filtered_cards, &num_cnts) {
+            Some(Self::Straight(st_cards)) => Some(Self::StraightFlush(st_cards)),
             None => None,
             _ => None,
         }
     }
 
-    fn is_four_of_a_kind(cards: &[Card]) -> Option<Self> {
+    fn is_four_of_a_kind(cards: &[Card], shape_cnts: &[usize; 4], num_cnts: &[usize; 13]) -> Option<Self> {
         // search a major num of the cards.
-        let num_cnts = Self::num_counts(cards);
         let four_kind: Option<_> = num_cnts
             .into_iter()
             .enumerate()
             .filter(|&(_num, num_cnt)| num_cnt == 4)
+	    .map(|(num, _num_cnt)| num)
             .nth(0);
 
         // check
         match four_kind {
-            Some((major_num, _)) => {
-                let another_highest = cards
-                    .into_iter()
-                    .cloned()
-                    .filter(|&card| card.num != major_num)
-                    .max_by_key(|&card| card.num)
-                    .map(|card| card.num)
-                    .unwrap();
-                Some(Self::FourOfAKind((
-                    major_num,
-                    major_num,
-                    major_num,
-                    major_num,
-                    another_highest,
-                )))
-            }
+            Some(major_num) => {
+		cards.sort_by_key(|&card| if card.num == major_num {card.num+13} else {card.num});
+		let sorted_cards = cards.into_iter().rev().take(5).collect_tuple().unwrap();
+		return Some(Self::FourOfAKind(sorted_cards));
+            },
             None => None,
         } // end of match
     } // end of is_four_of_a_kind
 
-    fn is_full_house(cards: &[Card]) -> Option<Self> {
+    fn is_full_house(cards: &[Card], shape_cnts: &[usize; 4], num_cnts: &[usize; 13]) -> Option<Self> {
         // search major nums of the cards.
-        let num_cnts = Self::num_counts(cards);
         // (3, 2, 1, 1) or (3, 2, 2) or (3, 3, 1) => full house
-        let triplets = num_cnts
-            .into_iter()
-            .enumerate()
+        let triplets_num = num_cnts.into_iter()..enumerate()
             .filter(|&(_num, num_cnt)| num_cnt == 3)
             .take(2)
-            .collect::<Vec<(usize, usize)>>();
+            .map(|(num, _num_cnt)| num)
+            .collect::<Vec<usize>>();
         if triplets.len() == 0 {
             return None;
         }
-        if triplets.len() == 2 {
+	let pairs_num = num_cnt.into_iter().enumerate().filter(|&(_num, num_cnt)| num_cnt == 2).take(2).map(|(num, _num_cnt)| num).collect::<Vec<usize>>();
+	if triplets.len() + pairs_num.len() <= 2 {return None;}
+
+	cards.sort_by_key(|&card| if triplets_num.contains(&card.num) {card.num+26} 
+			     else if pairs_num.contains(&card.num) {card.num+13} 
+				else {card.num});
+	let sorted_cards = cards.into_iter().rev().take(5).collect_tuple().unwrap();
+	return Some(Self::FullHouse(sorted_cards));
+        /*
+	if triplets.len() == 2 {
             // (3, 3, 1)
-            let higher_nums = cards
+            let major_cards = cards
                 .iter()
                 .cloned()
                 .filter(|&card| card.num == triplets[0].0 || card.num == triplets[1].0)
                 .take(5)
-                .map(|card| card.num)
                 .collect_tuple()
                 .unwrap();
-            return Some(Self::FullHouse(higher_nums));
+            return Some(Self::FullHouse(major_cards));
         }
         // if triplets.len() == 1
-        let high_pair_cnt = num_cnts
+        let higher_pair = num_cnts
             .into_iter()
             .enumerate()
             .filter(|&(_num, num_cnt)| num_cnt == 2)
             .max_by_key(|&(num, num_cnt)| num);
-        match high_pair_cnt {
-            Some((higher_p_num, _)) => Some(Self::FullHouse((
-                triplets[0].0,
-                triplets[0].0,
-                triplets[0].0,
-                higher_p_num,
-                higher_p_num,
-            ))),
+        match higher_pair {
+	    let major_cards = cards.iter().cloned().filter(|&card| card.num == triplets[0].0 || card.num == higher_pair).collect_tuple().unwrap(); 
+            Some((card1, card2)) => Some(Self::FullHouse(major_cards)),
             None => None,
-        }
+        }*/
     }
 
-    fn is_flush(cards: &[Card]) -> Option<Self> {
+    fn is_flush(cards: &[Card], shape_cnts: &[usize; 4], num_cnts: &[usize; 13]) -> Option<Self> {
         // grade : 5
-        let shape_cnts = Self::shape_counts(cards);
         let shcnt: Option<_> = shape_cnts
             .into_iter()
             .enumerate()
-            .filter(|&(_shape, shape_cnt)| shape_cnt >= 5)
+            .filter(|&(_shape, shape_cnt)| *shape_cnt >= 5)
             .nth(0);
         match shcnt {
             Some((major_shape, _)) => {
                 let highers = cards
-                    .into_iter()
-                    .cloned()
+                    .iter()
                     .filter(|&card| card.shape == major_shape)
                     .take(5)
-                    .map(|card| card.num)
                     .collect_tuple()
                     .unwrap();
                 Some(Self::Flush(highers))
@@ -262,117 +249,90 @@ impl Grade {
         }
     }
 
-    fn is_straight(cards: &[Card]) -> Option<Self> {
+    fn is_straight(cards: &[Card], num_cnts: &[usize; 13]) -> Option<Self> {
         // search straight nums.
-        let num_cnts = Self::num_counts(cards);
-        let mut cards = cards.to_vec();
-        cards.dedup(); // delete duplicates by card.num
-                       // must consider a special case : A2345 (0,1,2,3,12)
+        let mut cards = cards.to_vec().dedup();
+         // delete duplicates by card.num
+        // must consider a special case : A2345 (0,1,2,3,12)
         let mut consec_cnt: u8 = 0;
         let mut ace_exist: bool = false;
-        for (num, num_cnt) in num_cnts.into_iter().enumerate().rev() {
-            if num == 12 {
-                ace_exist = true;
-            }
-            if num_cnt == 0 {
-                consec_cnt = 0;
-            } else {
+        let str_lnum = num_cnts.into_iter().enumerate().rev().for_each(|(num, num_cnt)| {
+            if num == 12 	{ace_exist = true;}
+            if *num_cnt == 0 	{consec_cnt = 0;} 
+	    else {
                 consec_cnt += 1;
-                return match consec_cnt {
-                    5 => Some(Self::Straight((num + 4, num + 3, num + 2, num + 1, num))),
-                    4 if num == 0 && ace_exist => Some(Self::Straight((3, 2, 1, 0, 0))),
-                    _ => None,
-                };
-            }
-        }
-        return None;
+		if consec_cnt == 5 || (consec_cnt == 4 && num == 0 && ace_exist) {
+			num
+		}
+	});
+	match consec_cnt {
+                5 => {
+			cards.sort_by_key(|&card| if card.num >= str_lnum && card.num <= str_lnum + 4 {card.num + 13} else card.num);
+			let sorted_cards = cards.into_iter().rev().take(5).collect_tuple().unwrap();
+			return Some(Self::Straight(sorted_cards));
+		},
+                4 if str_lnum == 0 && ace_exist => {
+			cards.sort_by_key(|&card| if card.num >= 0 && card.num <= 3 {card.num + 13} else card.num)
+			let sorted_cards = cards.into_iter().rev().take(5).collect_tuple().unwrap();
+			return Some(Self::Straight(sorted_cards));
+	        },
+                _ => return None,
+        };
     }
 
-    fn is_three_of_a_kind(cards: &[Card]) -> Option<Self> {
+    fn is_three_of_a_kind(cards: &[Card], shape_cnts: &[usize; 4], num_cnts: &[usize; 13]) -> Option<Self> {
         // grade : 3
-        let num_cnts = Self::num_counts(cards);
         let num_triplet: Option<_> = num_cnts
             .into_iter()
             .enumerate()
-            .filter(|&(_num, num_cnt)| num_cnt == 3)
+            .filter(|&(_num, num_cnt)| *num_cnt == 3)
             .nth(0);
         match num_triplet {
             Some((num, _)) => {
-                let res_cards: (usize, usize) = cards
-                    .into_iter()
-                    .filter(|&card| card.num != num)
-                    .take(2)
-                    .map(|&card| card.num)
-                    .collect_tuple()
-                    .unwrap();
-                Some(Self::ThreeOfAKind((
-                    num,
-                    num,
-                    num,
-                    res_cards.0,
-                    res_cards.1,
-                )))
-            }
+		cards.sort_by_key(|&card| if card.num == num {13} else {card.num});
+		let sorted_cards = cards.into_iter().rev().take(5).collect_tuple().unwrap();
+                Some(Self::ThreeOfAKind(sorted_cards))
+            },
             None => None,
         }
     }
 
-    fn is_two_pairs(cards: &[Card]) -> Option<Self> {
+    fn is_two_pairs(cards: &'a [Card], shape_cnts: &'a [usize; 4], num_cnts: &'a [usize; 13]) -> Option<Self> {
         // grade : 2
         // (2, 2, 1, 1, 1), (2, 2, 2, 1)
-        let num_cnts = Self::num_counts(cards);
-        let num_pairs = num_cnts
-            .into_iter()
+        let pairs = num_cnts
+            .iter()
             .enumerate()
-            .filter(|&(_num, num_cnt)| num_cnt == 2)
+            .filter(|&(_num, num_cnt)| *num_cnt == 2)
             .rev()
             .take(3)
-            .collect::<Vec<(usize, usize)>>();
-        let num_pairs_len = num_pairs.len();
-        match num_pairs_len {
+            .collect::<Vec<(usize, &usize)>>();
+        let pairs_len = pairs.len();
+        match pairs_len {
             0..=1 => None,
             2..=3 => {
-                let res_num = cards
-                    .into_iter()
-                    .filter(|&card| card.num != num_pairs[0].0 && card.num != num_pairs[1].0)
-                    .nth(0)
-                    .unwrap()
-                    .num;
-                Some(Self::TwoPairs((
-                    num_pairs[0].0,
-                    num_pairs[0].0,
-                    num_pairs[1].0,
-                    num_pairs[1].0,
-                    res_num,
-                )))
-            }
+		cards.sort_by_key(|&card| if card.num == pairs[0].0 {14} else if card.num == pairs[1].0 {13} else {card.num});
+                let sorted_cards = cards.into_iter().rev().take(5).collect_tuple().unwrap();
+                Some(Self::TwoPairs(sorted_cards))
+            },
             _ => None,
         }
     }
 
-    fn is_one_pair(cards: &[Card]) -> Option<Self> {
+    fn is_one_pair(cards: &[Card], shape_cnts: &[usize; 4], num_cnts: &[usize; 13]) -> Option<Self> {
         // grade : 1
-        let num_cnts = Self::num_counts(cards);
-        let pair_num: Option<usize> = num_cnts
+        let pair_num = num_cnts
             .into_iter()
             .enumerate()
-            .filter(|&(_num, num_cnt)| num_cnt == 2)
-            .map(|(num, _num_cnt)| num)
+            .filter(|&(_num, num_cnt)| *num_cnt == 2)
             .nth(0);
         match pair_num {
             None => None,
-            Some(num) => {
-                let res_nums: (usize, usize, usize) = cards
-                    .into_iter()
-                    .filter(|&card| card.num != num)
-                    .map(|card| card.num)
-                    .take(3)
-                    .collect_tuple()
-                    .unwrap();
-                Some(Self::OnePair((
-                    num, num, res_nums.0, res_nums.1, res_nums.2,
-                )))
-            }
+            Some((num, _)) => {
+		cards.sort_by_key(|&card| if card.num == num {15} else {card.num});
+                let sorted_cards = cards.iter().rev().take(5).collect_tuple().unwrap();
+                Some(Self::OnePair(sorted_cards))
+            },
         }
     }
 }
